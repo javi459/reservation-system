9a96177b5b7019f85bf7cabaec9f5dc2e97495a4
javier <jaq459@gmail.com>
2023-01-17 17:43:28 -0600

    pushed files

---
+++ front-end/src/reservations/ReservationList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReservationCard from "./ReservationCard";
+
+// Defines the reservations list for the dashboard and search pages.
+
+export default function ReservationsList({ reservations, searchMode }) {
+  if (searchMode) {
+    const reservationsList = reservations.map((reservation) => {
+      return (
+        <ReservationCard
+          key={reservation.reservation_id}
+          reservation={reservation}
+        />
+      );
+    });
+    return <div>{reservationsList}</div>;
+  } else {
+    const reservationsList = reservations
+      .filter((reservation) => {
+        return reservation.status !== "cancelled";
+      })
+      .map((reservation) => {
+        return (
+          <ReservationCard
+            key={reservation.reservation_id}
+            reservation={reservation}
+          />
+        );
+      });
+    return <div>{reservationsList}</div>;
+  }
+}
