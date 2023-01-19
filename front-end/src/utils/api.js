80b86f56076ae1dfa1921899c5139aef553799e1
javi459 <103467214+javi459@users.noreply.github.com>
2023-01-18 20:21:54 -0600

    m

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,8 +6,6 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 import axios from "axios";
 
-const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || "https://reservation-system-backend.onrender.com";
 
 // const API_BASE_URL =
 //   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
