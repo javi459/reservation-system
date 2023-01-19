f37b5077e833b5aa1508d96e25d15cdc2e75dcb0
javi459 <103467214+javi459@users.noreply.github.com>
2023-01-18 23:41:43 -0600

    updatd routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -22,7 +22,7 @@ function Routes() {
   return (
     <Switch>
       <Route exact={true} path="/">
-      <Dashboard date={today()} />
+      <Dashboard date={today()} /> 
         {/* <Redirect to={"/dashboard"} /> */}
       </Route>
       <Route path="/dashboard">
