761599494491eb45efc667e9a2e7f8691268f9cf
javi459 <103467214+javi459@users.noreply.github.com>
2023-01-19 14:37:11 -0600

    m

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -13,7 +13,6 @@ const tablesRouter = require("./tables/tables.router");
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
